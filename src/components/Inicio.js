9b001ffad444479cc461dbbdc089f42216e28e99
JustKillu <nightcorekillu@gmail.com>
2023-11-28 20:04:07 -0400

    Se añade pagina de inicio de la pagina

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import algo from '../img/galaxyeyeswallpaper.jpg'; 
 
+const Intro = () => {
+  return (
+    <div>
+      <h1>Bienvenido a nuestra aplicación To Do</h1>
+      <p>Esta es una aplicación simple de To Do que te ayudará a mantener un registro de tus tareas pendientes. Puedes agregar nuevas tareas, editar tareas existentes y eliminar tareas que ya hayas completado. ¡Esperamos que encuentres esta aplicación útil!</p>
+      <img src={algo} alt="Imagen" />
+    </div>
+  );
+}
+
+export default Intro;
