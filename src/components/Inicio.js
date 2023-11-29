5b6ede8aa5595683041790534c9399788599b910
JustKillu <nightcorekillu@gmail.com>
2023-11-29 00:46:14 -0400

    Se añadio una descripcion de los creadores y css

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import algo from '../img/galaxyeyeswallpaper.jpg'; 
+import React, { useEffect, useState } from 'react';
+import '../styles/Inicio.css';
 
 const Intro = () => {
+  const [profiles, setProfiles] = useState([]);
+
+  useEffect(() => {
+    const users = ['JustKillu', 'Fervolts'];
+    Promise.all(users.map(user => 
+      fetch(`https://api.github.com/users/${user}`)
+        .then(response => response.json())
+    ))
+    .then(data => setProfiles(data))
+    .catch(err => console.error(err));
+  }, []);
+
   return (
-    <div>
+    <div className="intro">
       <h1>Bienvenido a nuestra aplicación To Do</h1>
       <p>Esta es una aplicación simple de To Do que te ayudará a mantener un registro de tus tareas pendientes. Puedes agregar nuevas tareas, editar tareas existentes y eliminar tareas que ya hayas completado. ¡Esperamos que encuentres esta aplicación útil!</p>
-      <img src={algo} alt="Imagen" />
+      <h2>Creadores:</h2>
+      <div className="profiles">
+        {profiles.map(profile => (
+          <div key={profile.id} className="profile-card">
+            <h2>{profile.name}</h2>
+            <p>{profile.bio}</p>
+            <a href={profile.html_url} target="_blank" rel="noopener noreferrer">
+              <img src={profile.avatar_url} alt={profile.name} />
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
