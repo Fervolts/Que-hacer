5826222630427c7b0d9ab7731e6b40ba9b8380a5
JustKillu <nightcorekillu@gmail.com>
2023-11-28 20:03:19 -0400

    Se añadio nuevas rutas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import Navb from "./components/Navb";
 import CrudApp from "./components/App";
+import InicioApp from "./components/Inicio";
+import ListaApp from "./components/TareasLista";
 
 export const App = () => {
   return (
@@ -10,8 +12,9 @@ export const App = () => {
 
       <div className="container">
         <Routes>
-          <Route path="/" element={<CrudApp/>}/>
-
+          <Route path="/" element={<InicioApp/>}/>
+          <Route path="/agregartasks" element={<CrudApp/>}/>
+          <Route path="/listas" element={<ListaApp/>}/>
         </Routes>
 
       </div>
