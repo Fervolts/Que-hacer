4f0c2d166c04a1060d789b655a10fc5f1cb4f749
Fervp <fernandocol2003@hotmail.com>
2023-11-29 01:11:41 -0400

    Se añaden retoques al inicio

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import algo from '../img/galaxyeyeswallpaper.jpg'; 
-
+import './Index.css'
 const Intro = () => {
   return (
     <div>
-      <h1>Bienvenido a nuestra aplicación To Do</h1>
-      <p>Esta es una aplicación simple de To Do que te ayudará a mantener un registro de tus tareas pendientes. Puedes agregar nuevas tareas, editar tareas existentes y eliminar tareas que ya hayas completado. ¡Esperamos que encuentres esta aplicación útil!</p>
-      <img src={algo} alt="Imagen" />
+    <div className='Box'>
+      <h1 className='Title'>¡Bienvenido a nuestra aplicación!</h1>
+      <p className='Text'>Esta es una aplicación simple de To Do que te ayudará a mantener un registro de tus tareas pendientes. Puedes agregar nuevas tareas, editar tareas existentes y eliminar tareas que ya hayas completado. ¡Esperamos que encuentres esta aplicación útil!</p>
+    </div>
+    <img src={algo} alt="Imagen" className='Img' />
     </div>
   );
 }
